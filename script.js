efb8311a961b45f2a8d697a81acc214e6c21849a
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-05-10 23:26:04 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// let elements=document.querySelectorAll("p");
+// console.dir(elements)
+// let headings=document.querySelectorAll(".heading");
+// console.dir(headings)
+// console.dir(document.body.firstChild);
+let heading=document.querySelector("h1");
